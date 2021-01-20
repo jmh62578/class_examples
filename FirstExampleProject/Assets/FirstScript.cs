921046acb632442acfa5c0b2cddf95c4d1b7d1aa
Kyle Johnsen <kjohnsen@uga.edu>
2021-01-20 14:09:05 -0500

    Added first example

---
+++ FirstExampleProject/Assets/FirstScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class FirstScript : MonoBehaviour
+{
+    
+    private void Start() //called once at the beginning (when you hit play, OR the object is first instantiated)
+    {
+        
+    }
+    void Update() //called once per frame
+    {
+
+        transform.Rotate(100*Time.deltaTime, 0, 0,Space.World);
+       
+    }
+    private void FixedUpdate()
+    {
+    }
+
+}
---
+++ FirstExampleProject/Assets/PrintTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PrintTransform : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Matrix4x4 toParent = this.transform.parent.worldToLocalMatrix * transform.localToWorldMatrix;
+        text.text = toParent + "";
+    }
+}
