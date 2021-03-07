71c0c6cf30fd91d41496433bd0ed9a3ca8e744b4
Kyle Johnsen <kjjohnsen@gmail.com>
2021-03-07 16:55:57 -0500

    from 3/4

---
+++ ExampleOculusQuestVR/Assets/WatchUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WatchUI : MonoBehaviour
+{
+    public Text watchText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setWatchText(Time.time + "");
+    }
+
+    public void setWatchText(string text)
+	{
+        watchText.text = text;
+	}
+}
