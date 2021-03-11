0c3682edbc8bcb1f8423252a98ed62ac5d326af8
kjjohnsen <kjohnsen@uga.edu>
2021-03-11 18:08:00 -0500

    added photon example, but make your own app id if you use...removed most of the demos and stuff

---
+++ PhotonStarter/Assets/NetworkManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class NetworkManager : MonoBehaviourPunCallbacks,ILobbyCallbacks
+{
+
+	public GameObject roomUI;
+	private void Awake()
+	{
+        Object.DontDestroyOnLoad(this);
+        Debug.Log("Connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+	}
+
+    public override void OnConnectedToMaster()
+	{
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+	}
+	public override void OnJoinedLobby()
+	{
+        Debug.Log("Joined Lobby");
+		roomUI.SetActive(true);
+	}
+	public override void OnRoomListUpdate(List<RoomInfo> roomList)
+	{
+		Debug.Log("Room list updated");
+		foreach(RoomInfo ri in roomList)
+		{
+			Debug.Log(ri.Name);
+		}
+	}
+
+	public void OnJoinRandomRoomClicked()
+	{
+		roomUI.SetActive(false);
+		PhotonNetwork.JoinRandomRoom();
+	}
+	public void OnCreateRoomClicked()
+	{
+		roomUI.SetActive(false);
+		PhotonNetwork.CreateRoom(null);
+	}
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		Debug.Log("Join Random Room Failed");
+		roomUI.SetActive(true);
+	}
+	public override void OnCreateRoomFailed(short returnCode, string message)
+	{
+		Debug.Log("Create Room Failed");
+		roomUI.SetActive(true);
+	}
+	public override void OnJoinedRoom()
+	{
+		Debug.Log("Joined Room");
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
